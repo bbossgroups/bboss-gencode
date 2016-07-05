c3d04d1e0e9aadabecd71aa428e67071fa1c83ba
yin-bp <yin-bp@163.com>
2016-07-05 23:33:46 +0800

    gradle改造

---
+++ gencode-core/src/org/frameworkset/gencode/core/IndexMethodBodyGenerate.java
@@ -0,0 +1,28 @@
+package org.frameworkset.gencode.core;
+
+import org.frameworkset.gencode.entity.Method;
+
+import bboss.org.apache.velocity.Template;
+import bboss.org.apache.velocity.VelocityContext;
+
+import com.frameworkset.util.VelocityUtil;
+
+public class IndexMethodBodyGenerate implements MethodBodyGenerate {
+
+	@Override
+	public void gen(Method method, String entityName, String entityVarName,
+			String paramName, String encodecharset, String exception,
+			int componentType, GencodeServiceImpl gencodeService)
+			throws Exception {
+		Template addmethodbodytempalte = VelocityUtil.getTemplate("gencode/java/body/index.vm");
+		 VelocityContext context = new VelocityContext();
+		 
+		 
+
+		 context.put("dbname", gencodeService.getDaoDBName());
+		 String body = GencodeServiceImpl.writetostring(context,addmethodbodytempalte,encodecharset);
+		 method.setBody(body);
+
+	}
+
+}
